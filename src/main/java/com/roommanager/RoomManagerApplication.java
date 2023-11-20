5858c7e4bab3869c46588e582c87b70932ab9e15
Artiom Iurcovschi <artiom.08@gmail.com>
2023-11-20 18:22:15 +0200

    project init commit

---
+++ src/main/java/com/roommanager/RoomManagerApplication.java
@@ -0,0 +1,13 @@
+package com.roommanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomManagerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RoomManagerApplication.class, args);
+  }
+
+}
